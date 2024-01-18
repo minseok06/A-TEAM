9ebf61348ab36dc3da683aeca43a3cd868d08274
양민석 | Yang Minseok <121544294+minseok06@users.noreply.github.com>
2024-01-18 14:56:50 +0900

    Delete A TEAM directory

--- A TEAM/js/script.js
+++
@@ -1,8 +0,0 @@
-$('.navi>li').mouseover(function(){
-    $(this).children('.submenu').stop().slideDown(300);
-    // $('.submenu').stop().slideDown(200);
-});
-$('.navi>li').mouseout(function(){
-    $(this).children('.submenu').stop().slideUp(200);
-    // $('.submenu').stop().slideUp(200);
-});
